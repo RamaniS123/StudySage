00be24f51687dab5de1c2ad56ddc5fd182b2ecd0
Ramani Satishkumar <ramanisatishkumar2005@gmail.com>
2025-08-04 18:00:00 -0400

    Update quiz page

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import QuizClient from "@/components/QuizClient";
 
 export default function QuizPage() {
   return (
     <div className="p-4">
-      <QuizClient />
+      <Suspense fallback={<p>Loading quiz...</p>}>
+        <QuizClient />
+      </Suspense>
     </div>
   );
 }
