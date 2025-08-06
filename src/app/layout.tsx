aa9880b9cddc19f16da870ceb0ab811c9927f474
Ramani Satishkumar <ramanisatishkumar2005@gmail.com>
2025-08-05 22:56:57 -0400

    Fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,18 +26,16 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <NoteProvider>
-            <SidebarProvider>
-              <AppSidebar />
-              <div className="flex min-h-screen w-full flex-col">
-                <Header />
-                <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
-                  {children}
-                </main>
-              </div>
-            </SidebarProvider>
-            <Toaster />
-          </NoteProvider>
+          <SidebarProvider>
+            <AppSidebar />
+            <div className="flex min-h-screen w-full flex-col">
+              <Header />
+              <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+                {children}
+              </main>
+            </div>
+          </SidebarProvider>
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
